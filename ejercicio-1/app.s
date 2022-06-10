b3271d33a7c663db5c96f833538ed32ea0a1ec16
MolinaFranco <franco13mm@gmail.com>
2022-06-09 23:50:46 -0300

    fixeado overflow

--- ejercicio-1/app.s
+++ ejercicio-1/app.s
@@ -9,14 +9,13 @@ main:
 	// X0 contiene la direccion base del framebuffer
  	mov x20, x0	// Save framebuffer base address to x20
 	//---------------- CODE HERE ------------------------------------
-	movz 	x10, 0xff, lsl 16
-	movk 	x10, 0xffff, lsl 00
+	movz 	x10, 0x00, lsl 16
+	movk 	x10, 0x0000, lsl 00
 	mov x1, #0
 	mov x2, #0
-	mov x3, #640
-	mov x4, #480
+	mov x3, SCREEN_WIDTH
+	mov x4, SCREEN_HEIGH
 	bl doRectangulo
-
 
 
 	mov x1, #0
